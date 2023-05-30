1b8215e41a5ba2bee156167bab972528279b84ff
Satwik Kar <satwik.k.2000@gmail.com>
2023-05-30 23:49:04 +0530

    made bar-chart partial

--- app/src/main/java/com/spendwise/HomeActivity.kt
+++ app/src/main/java/com/spendwise/HomeActivity.kt
@@ -4,17 +4,21 @@ package com.spendwise
 import android.app.Activity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 
 class HomeActivity : Activity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val datas = listOf<Float>(500f, 200f, 300f, 500f, 300f)
-        val barView = BarChartView(applicationContext, datas)
-
-        val barChartLayout = findViewById<LinearLayout>(R.id.linearLayoutBarChart)
-
-        barChartLayout.addView(barView)
+        val bar1 = findViewById<CardView>(R.id.bar1)
+        val bar2 = findViewById<CardView>(R.id.bar2)
+        val bar3 = findViewById<CardView>(R.id.bar3)
+        val bar4 = findViewById<CardView>(R.id.bar4)
+        val bar5 = findViewById<CardView>(R.id.bar5)
+        bar1.minimumHeight = 50
+        bar2.minimumHeight = 25
 
 
     }
