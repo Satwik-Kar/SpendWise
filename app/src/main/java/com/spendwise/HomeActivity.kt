78c322eac49a8bfe6706aaa75ceb2bac2bf231ca
Satwik Kar <satwik.k.2000@gmail.com>
2023-06-25 19:50:11 +0530

    added-add-new-btn-functionalities

--- app/src/main/java/com/spendwise/HomeActivity.kt
+++ app/src/main/java/com/spendwise/HomeActivity.kt
@@ -3,20 +3,29 @@ package com.spendwise
 
 import android.animation.ValueAnimator
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class HomeActivity : Activity() {
     lateinit var barLinearLayout: LinearLayout
+    lateinit var addNewBtn: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        barLinearLayout = findViewById(R.id.barLinearLayout)
+        barLinearLayout = this.findViewById(R.id.barLinearLayout)
+        addNewBtn = this.findViewById(R.id.addNew)
         val animatedView = BarChartView(this, arrayListOf<Float>(100f,200f,300f,400f,500f),
             arrayListOf("s","s","s","s","s")
         )
         barLinearLayout.addView(animatedView)
+        addNewBtn.setOnClickListener {
+            val intent = Intent(this@HomeActivity,AddNew::class.java)
+            startActivity(intent)
+        }
 
 
 
