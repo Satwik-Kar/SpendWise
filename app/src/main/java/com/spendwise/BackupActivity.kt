7be9ae603c0ad972b7a47da8c35693544bbd2492
Satwik Kar <satwik.k.2000@gmail.com>
2023-08-10 14:54:12 +0530

    added offline backup support

--- app/src/main/java/com/spendwise/BackupActivity.kt
+++ app/src/main/java/com/spendwise/BackupActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.icu.util.Calendar
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,11 +16,14 @@ import java.io.FileInputStream
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
+
 class BackupActivity : AppCompatActivity() {
     private lateinit var lastBackupLocal: TextView
     private lateinit var lastBackupLocalSize: TextView
     private lateinit var backupButton: Button
-    private val REQUEST_CODE = 122
+    private lateinit var restoreButton: Button
+    private val REQUEST_CODE_BACKUP = 122
+    private val REQUEST_CODE_RESTORE = 123
     private lateinit var BACKUP_TYPE: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +32,17 @@ class BackupActivity : AppCompatActivity() {
         lastBackupLocal = this.findViewById(R.id.dateOfLocalBackup)
         lastBackupLocalSize = this.findViewById(R.id.sizeOfBackup)
         backupButton = this.findViewById(R.id.backUpButton)
+        restoreButton = this.findViewById(R.id.restoreButton)
         val size =
             getSharedPreferences("credentials", MODE_PRIVATE).getString("last_backup_size", null)
 
-        val dateTimeType = getSharedPreferences("credentials", MODE_PRIVATE)
-            .getString("last_backup_date", null)
+        val dateTimeType =
+            getSharedPreferences("credentials", MODE_PRIVATE).getString("last_backup_date", null)
         if (size != null) {
             lastBackupLocalSize.text = size
 
         } else {
-            lastBackupLocalSize.text = "Size: N/A"
+            lastBackupLocalSize.visibility = View.INVISIBLE
         }
         if (dateTimeType != null) {
             lastBackupLocal.text = dateTimeType
@@ -50,6 +56,10 @@ class BackupActivity : AppCompatActivity() {
             this.initiateBackupInDevice()
 
         }
+        restoreButton.setOnClickListener {
+
+            this.initiateRestoreInDevice()
+        }
 
 
     }
@@ -58,9 +68,21 @@ class BackupActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.type = "application/octet-stream"
-        intent.putExtra(Intent.EXTRA_TITLE, Constants.BACKUP_FILE_NAME)
+        intent.putExtra(
+            Intent.EXTRA_TITLE,
+            "${
+                getDateTime().replace(Regex("[/:\\s]"), "")
+            }_${Constants.BACKUP_FILE_NAME}"
+        )
+        startActivityForResult(intent, REQUEST_CODE_BACKUP)
+    }
 
-        startActivityForResult(intent, REQUEST_CODE)
+    private fun initiateRestoreInDevice() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+
+        startActivityForResult(intent, REQUEST_CODE_RESTORE)
     }
 
     private fun performBackup(uri: Uri) {
@@ -94,9 +116,7 @@ class BackupActivity : AppCompatActivity() {
                 lastBackupLocal.text = dateTimeType
 
                 Toast.makeText(
-                    this@BackupActivity,
-                    "Backup Successful.",
-                    Toast.LENGTH_LONG
+                    this@BackupActivity, "Backup Successful.", Toast.LENGTH_LONG
                 ).show()
 
             }
@@ -104,11 +124,79 @@ class BackupActivity : AppCompatActivity() {
 
     }
 
+    private fun performRestore(uri: Uri) {
+        val sourceDBPath = applicationContext.getDatabasePath(Constants.DATABASE_NAME).absolutePath
+        val sourceDBFile = File(sourceDBPath)
+        if (isValidSQLiteFile(uri)) {
+            try {
+                val contentResolver = applicationContext.contentResolver
+                val inputStream = contentResolver.openInputStream(uri)
+
+                if (inputStream != null) {
+                    sourceDBFile.outputStream().use { output ->
+                        inputStream.copyTo(output, bufferSize = 1024)
+                    }
+
+                    Toast.makeText(
+                        this@BackupActivity, "Database restored successfully!", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this@BackupActivity,
+                        "Unable to open input stream for the selected file",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(
+                    this@BackupActivity,
+                    "Error restoring the database: ${e.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                this@BackupActivity,
+                "This is not a valid backup file for SpendWise",
+                Toast.LENGTH_LONG
+            ).show()
+
+
+        }
+
+
+    }
+
+
+    private fun isValidSQLiteFile(fileUri: Uri): Boolean {
+
+
+        val fileExtension = MimeTypeMap.getFileExtensionFromUrl(fileUri.toString())
+        if (fileExtension != null && fileExtension.equals("db", ignoreCase = true)) {
+
+            val mimeType = contentResolver.getType(fileUri)
+            if (mimeType != null && mimeType == "application/octet-stream") {
+
+                return true
+            }
+        }
+        return false
+
+
+    }
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        if (requestCode == REQUEST_CODE_BACKUP && resultCode == Activity.RESULT_OK) {
             data?.data?.let { uri ->
                 performBackup(uri)
+            }
+        }
+        if (requestCode == REQUEST_CODE_RESTORE && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                performRestore(uri)
             }
         }
     }
@@ -117,10 +205,9 @@ class BackupActivity : AppCompatActivity() {
 
 
         val decimalPlaces = 2.0
-        val roundedVal =
-            ((size) * 10.0.pow(decimalPlaces)).roundToInt() / 10.0.pow(
-                decimalPlaces
-            )
+        val roundedVal = ((size) * 10.0.pow(decimalPlaces)).roundToInt() / 10.0.pow(
+            decimalPlaces
+        )
 
         return "${roundedVal}"
 
@@ -128,12 +215,7 @@ class BackupActivity : AppCompatActivity() {
     }
 
     private fun formatDate(
-        year: Int,
-        month: Int,
-        day: Int,
-        hrs: Int,
-        mins: Int,
-        secs: Int
+        year: Int, month: Int, day: Int, hrs: Int, mins: Int, secs: Int
     ): String {
         val formattedMonth = if (month + 1 < 10) "0${month + 1}" else "${month + 1}"
         val formattedDay = if (day < 10) "0$day" else "$day"
